9a3a0a94c225c495b77216f9b61578f528fb5c3d
RATSENO <onestar1014@gmail.com>
2020-02-06 15:35:11 +0900

    [MOD]상세 수정페이지 작성

--- src/main/java/com/bd/controller/PageController.java
+++ src/main/java/com/bd/controller/PageController.java
@@ -2,12 +2,31 @@ package com.bd.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PageController {
 
 	@GetMapping("/")
-	public String depth1() {
+	public String depth() {
 		return "login";
 	}
+	
+	@GetMapping("/{depth1}")
+	public String depth1(@PathVariable(name = "depth1")String depth1) {
+		return depth1;
+	}
+	
+	@GetMapping("/{depth1}/{depth2}")
+	public String depth2(@PathVariable(name = "depth1")String depth1,
+						 @PathVariable(name = "depth2")String depth2) {
+		return depth1 + "/" + depth2;
+	}
+	
+	@GetMapping("/{depth1}/{depth2}/*")
+	public String error() {
+		return "error";
+	}
+	
+	
 }
