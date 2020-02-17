45147c54f1ed0ad0c2fa408bd682830f0eed8088
RATSENO <onestar1014@gmail.com>
2020-02-17 15:41:55 +0900

    file upload, download 간단 예제
    https://pangsblog.tistory.com/68?category=807782 참고

--- src/main/java/com/bd/CompanyProjectApplication.java
+++ src/main/java/com/bd/CompanyProjectApplication.java
@@ -2,8 +2,12 @@ package com.bd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.bd.config.FileUploadConfig;
 
 @SpringBootApplication
+@EnableConfigurationProperties(FileUploadConfig.class)
 public class CompanyProjectApplication {
 
 	public static void main(String[] args) {
